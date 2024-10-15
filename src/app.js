83bda8a65308b172eeff6168b123e403d4c2dd3f
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2024-10-15 12:46:59 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,20 +8,7 @@ const port = process.env.PORT || 4000;
 app.use(cors({ origin: '*', credentials: true }));
 app.options('*', cors({ origin: '*', credentials: true }));
 
-// app.use(cors({ origin: `http://localhost:3001`, credentials: true }));
-// app.options('*', cors({ origin: 'http://localhost:3001', credentials: true }));
-
 app.use((req, res, next) => {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-
-    let allowedDomains = ['http://localhost:3000', 'http://192.168.0.147:4000', 'https://insta-web.netlify.app' ];
-    let origin = req.headers.origin;
-    
-    if(allowedDomains.indexOf(origin) > -1){
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Content-Type', 'application/json'); 
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
